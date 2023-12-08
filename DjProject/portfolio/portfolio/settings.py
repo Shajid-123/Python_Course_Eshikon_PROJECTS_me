7d0d73bef6761dc4fa698eca19e0cd04d950a1f7
Shan Shajid Shanza <sathishanza@gmail.com.com>
2023-12-08 20:24:49 +0600

    comment

--- DjProject/portfolio/portfolio/settings.py
+++ DjProject/portfolio/portfolio/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+temp_dir = os.path.join(BASE_DIR, 'templates')
+static_dir = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [temp_dir],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [static_dir,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- DjProject/portfolio/portfolio/views.py
+++ DjProject/portfolio/portfolio/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def demo(request):
-    return HttpResponse("This is a root url function")
+    name = "Shoaib Rahman Shajid"
+    demo_list = ["Lemborghini Essenza SV12","Buggati Bolide W16.4"]
+    dict = {"name":name,"list":demo_list}
+    return render(request, 'portfolio.html')
